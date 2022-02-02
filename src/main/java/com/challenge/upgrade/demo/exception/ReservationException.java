d766f9d38e2dc34513545bf2acf2478c44f55201
guille <exarodriguez@gmail.com>
2022-02-02 18:35:58 -0300

    Fix application.properties url

--- src/main/java/com/challenge/upgrade/demo/exception/ReservationException.java
+++ src/main/java/com/challenge/upgrade/demo/exception/ReservationException.java
@@ -7,10 +7,6 @@ public class ReservationException extends RuntimeException {
      */
     private static final long serialVersionUID = 5629901113497598092L;
 
-    public ReservationException(String message, Throwable cause){
-        super(message, cause);
-    }
-
     public ReservationException(String message){
         super(message);
     }
